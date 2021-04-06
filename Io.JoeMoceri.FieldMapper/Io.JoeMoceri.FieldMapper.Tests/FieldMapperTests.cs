f4b913d50a0d519783b3163cb9873d48730945da
Joe Moceri <jmoceri34@gmail.com>
2021-04-06 00:53:03 -0700

    changes

--- Io.JoeMoceri.FieldMapper/Io.JoeMoceri.FieldMapper.Tests/FieldMapperTests.cs
+++ Io.JoeMoceri.FieldMapper/Io.JoeMoceri.FieldMapper.Tests/FieldMapperTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace FieldMapper.Tests
+namespace Io.JoeMoceri.FieldMapper.Tests
 {
     [TestClass]
     public class FieldMapperTests

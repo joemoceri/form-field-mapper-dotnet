658e12a70eb9cda35ac7706a2b9e246420d6d005
Joe Moceri <jmoceri34@gmail.com>
2021-07-31 19:43:30 -0700

    changes

--- src/FieldMapperForDotNet.Sample/App.cs
+++ src/FieldMapperForDotNet.Sample/App.cs
@@ -10,7 +10,16 @@ namespace FieldMapperForDotNet.Sample
     {
         public void Run()
         {
+            var fieldMapper = new FieldMapper();
+            var content = $"First Name: TestFirstName{Environment.NewLine}Last Name: TestLastName";
+            var mappings = new List<string>();
+            mappings.Add("First Name:");
+            mappings.Add("Last Name:");
 
+            var result = fieldMapper.Get(content, mappings);
+
+            var firstName = result["First Name:"]; // outputs TestFirstName
+            var lastName = result["Last Name:"]; // outputs TestLastName
         }
     }
 }

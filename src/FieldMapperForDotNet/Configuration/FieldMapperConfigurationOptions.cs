0646feaaa155a6b6f005c48786214b59f7073657
Daerius-rs <dima.armawir@yandex.ru>
2021-08-03 08:30:05 +0300

    Main: Add Default property in FieldMapperConfigurationOptions class & Update in-code documentation in FieldMapperConfigurationOptions class

--- src/FieldMapperForDotNet/Configuration/FieldMapperConfigurationOptions.cs
+++ src/FieldMapperForDotNet/Configuration/FieldMapperConfigurationOptions.cs
@@ -6,12 +6,27 @@
     public class FieldMapperConfigurationOptions
     {
         /// <summary>
-        /// This will DeEntitize the content by stripping all html and other relevant characters. If you're pulling plain-text, you can set this to false most likely. If you're pulling html strings, you may want it true.
+        /// Returns default options.
+        /// </summary>
+        public static FieldMapperConfigurationOptions Default
+        {
+            get
+            {
+                return new FieldMapperConfigurationOptions
+                {
+                    DeEntitizeContent = true,
+                    SeparateByLineBreaks = true
+                };
+            }
+        }
+
+        /// <summary>
+        /// This will DeEntitize the content by stripping all html and other relevant characters. If you're pulling plain-text, you can set this to <see langword="false"/> most likely. If you're pulling html strings, you may want it <see langword="true"/>.
         /// </summary>
         public bool DeEntitizeContent { get; set; }
 
         /// <summary>
-        /// This keeps everything clean, recommend leaving as true
+        /// This keeps everything clean, recommend leaving as <see langword="true"/>.
         /// </summary>
         public bool SeparateByLineBreaks { get; set; }
     }

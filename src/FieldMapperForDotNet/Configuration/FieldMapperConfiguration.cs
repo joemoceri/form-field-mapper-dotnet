570acf92b0d6f629b33a7deea9245312c21c0ccd
Daerius-rs <dima.armawir@yandex.ru>
2021-08-03 08:35:49 +0300

    Main: Add Default property in FieldMapperConfiguration class & Add constructor to which you can pass options & Update in-code documentation in FieldMapperConfiguration class

--- src/FieldMapperForDotNet/Configuration/FieldMapperConfiguration.cs
+++ src/FieldMapperForDotNet/Configuration/FieldMapperConfiguration.cs
@@ -6,20 +6,36 @@
     public class FieldMapperConfiguration
     {
         /// <summary>
-        /// You can modify the options to change how the field mapper works when running <see cref="FieldMapper.Get(string, System.Collections.Generic.IEnumerable{string})"/>
+        /// Return default configuration.
         /// </summary>
-        public readonly FieldMapperConfigurationOptions options;
+        public static FieldMapperConfiguration Default
+        {
+            get
+            {
+                return new FieldMapperConfiguration();
+            }
+        }
 
         /// <summary>
-        /// By default initalizes the options with <see cref="FieldMapperConfigurationOptions.DeEntitizeContent"/> as true and <see cref="FieldMapperConfigurationOptions.SeparateByLineBreaks"/> as true.
+        /// You can modify the options to change how the field mapper works when running <see cref="FieldMapper.Get(string, System.Collections.Generic.IEnumerable{string})"/>.
+        /// </summary>
+        public readonly FieldMapperConfigurationOptions Options;
+
+        /// <summary>
+        /// By default initializes the options with <see cref="FieldMapperConfigurationOptions.DeEntitizeContent"/> as <see langword="true"/> and <see cref="FieldMapperConfigurationOptions.SeparateByLineBreaks"/> as <see langword="true"/>.
         /// </summary>
         public FieldMapperConfiguration()
+            : this(FieldMapperConfigurationOptions.Default)
         {
-            options = new FieldMapperConfigurationOptions
-            {
-                DeEntitizeContent = true,
-                SeparateByLineBreaks = true
-            };
+
+        }
+        /// <summary>
+        /// You can pass in your own <see cref="FieldMapperConfigurationOptions"/>. By default initializes the options with <see cref="FieldMapperConfigurationOptions.DeEntitizeContent"/> as <see langword="true"/> and <see cref="FieldMapperConfigurationOptions.SeparateByLineBreaks"/> as <see langword="true"/>.
+        /// </summary>
+        /// /// <param name="options">Your own <see cref="FieldMapperConfigurationOptions"/></param>
+        public FieldMapperConfiguration(FieldMapperConfigurationOptions options)
+        {
+            Options = options ?? FieldMapperConfigurationOptions.Default;
         }
     }
 }
